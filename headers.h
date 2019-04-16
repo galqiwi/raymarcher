74ac73755eff16e9764a3979dbd53c58126b29fe
Galqiwi <galqiwi@gmail.com>
2019-04-17 01:46:22 +0300

    opencv added

--- headers.h
+++ headers.h
@@ -5,6 +5,14 @@
 #include <fstream>
 #include <cmath>
 
+#include <opencv2/core/types_c.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/videoio/videoio.hpp>
+#include <opencv2/videoio/registry.hpp>
+
 using namespace std;
+using namespace cv;
 
 #endif
